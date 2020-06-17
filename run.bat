fb2f37ae1d2bde08a4bb5870498c56d9c472b75c
gabroo <sajeevsa@usc.edu>
2020-06-17 00:16:39 -0700

    delete non-unix scripts

--- run.bat
+++
@@ -1,3 +0,0 @@
-set PYTHONPATH=C:\cc3d_custom\lib\site-packages
-set PYTHON_INSTALL_PATH=C:\cc3d_custom\python36
-%PYTHON_INSTALL_PATH%\python.exe run.py
--- run.sh
+++
@@ -1,8 +0,0 @@
-echo "starting job in run.sh ..."
-export PREFIX_CC3D=../cc3d_420
-export PYTHON_INSTALL_PATH=${PREFIX_CC3D}/Python37/bin
-export PYTHONPATH=${PREFIX_CC3D}/lib/site-packages
-export PATH=$PATH:PYTHON_INSTALL_PATH
-export CPP=${PYTHONPATH}/cc3d/cpp
-export LD_LIBRARY_PATH=${PREFIX_CC3D}/lib/:${CPP}/lib:${CPP}/CompuCell3DPlugins:${CPP}/CompuCell3DSteppables:$LD_LIBRARY_PATH
-python3 run.py
